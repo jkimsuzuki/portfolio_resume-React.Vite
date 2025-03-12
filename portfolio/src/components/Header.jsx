be33f02668a4553426be0d2494422b61d3e8e2e0
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-13 06:46:27 +0900

    Set base for Header and Navbar

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -4,8 +4,20 @@
  */
 
 
+/**
+ * Node modules
+ */
+import { useState } from "react";
+
+
+/**
+ * Components
+ */
+import Navbar from "./Navbar"
 
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
         <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
@@ -34,12 +46,12 @@ const Header = () => {
                </span>
              </button>
 
-             Navbar
+             <Navbar navOpen={navOpen} />
           </div>   
 
           <a 
             href="#contact"
-            className=""
+            className="btn btn-secondary max-md:hidden md:justify-self-end"
           >
             Contact Me
           </a>    
