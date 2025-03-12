dd0f0bb2b92e819bb93ea077d9408c4cbabe3a9c
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-13 01:23:24 +0900

    Fix Google Fonts api link on index.html to show all fonts

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
         <div className="max-w-screen-2xl w-full mx-auto px-4">
 
-         <h1>
+          <h1>
             <a 
             href="/" 
             className="logo"
@@ -22,7 +22,18 @@ const Header = () => {
               alt="Joseph Kim-Suzuki" 
               />
             </a>
-         </h1>
+          </h1>
+
+          <div className="">
+             <button
+             className=""
+             onClick={null}
+             >
+               <span className="material-symbols-rounded">
+                menu
+               </span>
+             </button>
+          </div>       
 
         </div>
     </header>
