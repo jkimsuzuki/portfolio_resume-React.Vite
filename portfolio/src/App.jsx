d0f9c449f1cbca303523a77a50208808405f7a0e
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-13 21:21:06 +0900

    Finish draft for About section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,7 +9,7 @@
  */
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-
+import About from "./components/About";
 
 const App = () => {
 
@@ -18,6 +18,7 @@ const App = () => {
        <Header />
        <main>
         <Hero />
+        <About />
        </main>
     </>
    
