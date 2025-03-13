b30517e05af9b845a4d0337537add017af1c5038
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-13 17:33:51 +0900

    Get Available for work sign light marker to pulse, portrait background gradient and round off

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -8,14 +8,21 @@
  * Components
  */
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 
 const App = () => {
 
   return (
-    <Header />
+    <>
+       <Header />
+       <main>
+        <Hero />
+       </main>
+    </>
+   
   )
-  
+
 }
 
 
