1fcf6e764fd3ef3a99aa0fd41f8e17864232de59
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-13 19:12:02 +0900

    Create button component and finish 'Download CV' button

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -4,6 +4,12 @@
  */
 
 
+/**
+ * Components
+ */
+import { ButtonPrimary } from "./Button"
+
+
 const Hero = () => {
   return (
     <section 
@@ -38,7 +44,10 @@ const Hero = () => {
             </h2>
            
             <div className="flex items-center gap-3">
-              ButtonPrimary
+              <ButtonPrimary 
+                label="Download CV"
+                icon="download"
+              />
 
               ButtonOutline
             </div>
