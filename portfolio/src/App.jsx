6019999fab7d23e3e72b83789e2eff0847a0eb43
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-13 21:55:22 +0900

    Add Skill component and create heading/text section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -10,6 +10,8 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skill from "./components/Skill";
+
 
 const App = () => {
 
@@ -19,6 +21,7 @@ const App = () => {
        <main>
         <Hero />
         <About />
+        <Skill />
        </main>
     </>
    
