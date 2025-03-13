e96693de4efc8a25dd72a04a469638ee8941cdc7
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-14 08:18:47 +0900

    Add ProjectCard component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -11,6 +11,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Hero />
         <About />
         <Skill />
+        <Work />
        </main>
     </>
    
---
+++ portfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,14 @@
+/**
+ * @copyright 2025 Joseph Yu Kim-Suzuki
+ * @license Apache-2.0
+ */
+
+const ProjectCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProjectCard
