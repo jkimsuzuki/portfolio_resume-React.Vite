bcba2af5821656645d9719c038e5092a44a518af
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-14 13:33:37 +0900

    Get general layout of Contact form

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,6 +12,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
         <About />
         <Skill />
         <Work />
+        <Contact />
        </main>
     </>
    
