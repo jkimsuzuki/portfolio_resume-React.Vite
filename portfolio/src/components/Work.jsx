21f1e2ed4c28c88fbb810e1f859df15833605048
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-14 09:23:29 +0900

    Finish draft of Work component with ProjectCards

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -4,6 +4,12 @@
  */
 
 
+/**
+ * Components
+ */
+import ProjectCard from "./ProjectCard";
+
+
 const works = [
     {
       imgSrc: '/images/project-1.jpg',
@@ -56,9 +62,15 @@ const Work = () => {
           My portfolio highlights
         </h2>
 
-        <div className="">
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
             {works.map(({ imgSrc, title, tags, projectLink }, key) => (
-                'ProjectCard'
+                <ProjectCard
+                key={key}
+                imgSrc={imgSrc}
+                title={title}
+                tags={tags}
+                projectLink={projectLink}
+                />
             ))}
         </div>
 
