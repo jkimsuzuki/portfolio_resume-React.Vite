e2d9774419075f00fc9e8f7e875c621f1ecbfb71
Joseph Kim-Suzuki <jkimsuzuki@gmail.com>
2025-03-14 15:56:25 +0900

    Add GSAP scroll animation Step 1

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,7 +7,16 @@
 /**
  * Node modules
  */
-import { ReactLenis } from 'lenis/react'
+import { ReactLenis } from 'lenis/react';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+
+/**
+ * Node modules
+ */
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 
 /**
@@ -22,7 +31,28 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 
+
 const App = () => {
+
+  useGSAP(() => {
+    const elements = gsap.utils.toArray('.reveal-up');
+
+    elements.forEach((element) => {
+      gsap.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: '-200 bottom',
+          end: 'bottom 80%',
+          scrub: true,
+          markers: true
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out'
+      })
+    });
+  });
 
   return (
     <ReactLenis root>
